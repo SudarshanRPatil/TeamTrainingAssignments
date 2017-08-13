d1fa0e8d37ab150d8009c218a0ed335581219ce6
SudarshanRPatil <mailtosudarshan7891@gmail.com>
2017-08-13 20:55:31 +0530

    DayThreeAss complete

---
+++ DayThreeAssignment/ConsoleApp/ConsoleApp/SimpleCache.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp
+{
+    public class SimpleCache
+    {
+        private static readonly Dictionary<string, CacheItem> Cache = new Dictionary<string, CacheItem>();
+
+        public void Add(string key, object value)
+        {
+            Cache.Add(key, new CacheItem() { Value = value, Created = DateTime.Now.Ticks });
+        }
+
+        public object this[string key]
+        {
+            get
+            {
+                if (Cache.ContainsKey(key))
+                {
+                    if ((Cache[key].Created + 30000) > DateTime.Now.Ticks)
+                    {
+                        return Cache[key].Value;
+                    }
+                    Cache.Remove(key);
+                }
+                return null;
+            }
+        }
+
+        public bool Clear(string key)
+        {
+            return Cache.Remove(key);
+        }
+
+        public void Update(string key, object value)
+        {
+            CacheItem cacheItem = new CacheItem();
+            cacheItem.Value = value;
+            Cache[key] = cacheItem;
+        }
+
+        public class CacheItem
+        {
+            public object Value { get; set; }
+            public long Created { get; set; }
+        }
+
+        public object Retrive(string key)
+        {
+            CacheItem cacheItem;
+            if (Cache.TryGetValue(key, out cacheItem) == false)
+            {
+                return string.Format("Invalid or key not found: {0}", key); 
+            }
+            return cacheItem.Value;
+        }
+    }
+}
---
+++ DayThreeAssignment/ConsoleApp/ConsoleAppTestProject/SimpleCacheTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ConsoleApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleAppTestProject
+{
+    [TestClass]
+    public class SimpleCacheTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            Employee employee1 = new Employee();
+            employee1.Age = 26;
+            employee1.Name = "XYZ";
+
+            SimpleCache simpleCache = new SimpleCache();
+
+            simpleCache.Add("one",employee1);
+
+            Assert.IsNotNull(simpleCache);
+
+            Debug.WriteLine("Key = {0}, Value = {1}", "one", simpleCache.Retrive("one").GetType());
+        }
+
+        [TestMethod]
+        public void ClearTest()
+        {
+            Employee employee1 = new Employee();
+            employee1.Age = 26;
+            employee1.Name = "XYZ";
+
+            SimpleCache simpleCache = new SimpleCache();
+            simpleCache.Add("one", employee1);
+
+            simpleCache.Clear("one");
+
+            Assert.IsNotNull(simpleCache);
+
+            Debug.WriteLine("Key = {0}, Value = {1}", "one", simpleCache.Retrive("one"));
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            Employee employee1 = new Employee();
+            employee1.Age = 26;
+            employee1.Name = "XYZ";
+
+            SimpleCache simpleCache = new SimpleCache();
+            simpleCache.Add("one", employee1);
+            var emp = (Employee)simpleCache.Retrive("one");
+            Assert.IsNotNull(simpleCache);
+            Debug.WriteLine("Key = {0}, Employee Value Name = {1} Age = {2}", "one", emp.Name, emp.Age);
+
+            employee1.Name = "PQR";
+            simpleCache.Update("one", employee1);
+            emp = (Employee)simpleCache.Retrive("one");
+            Assert.IsNotNull(simpleCache);
+            Debug.WriteLine("Key = {0}, Employee Value Name = {1} Age = {2}", "one", emp.Name, emp.Age);
+        }
+
+        [TestMethod]
+        public void RetriveTest()
+        {
+            Employee employee1 = new Employee();
+            employee1.Age = 26;
+            employee1.Name = "XYZ";
+
+            SimpleCache simpleCache = new SimpleCache();
+            simpleCache.Add("one", employee1);
+
+            var value = (Employee)simpleCache.Retrive("one");
+            Assert.IsNotNull(simpleCache);
+            Debug.WriteLine("Employee Name = {0}, Age = {1}", value.Name, value.Age);
+        }
+    }
+}
